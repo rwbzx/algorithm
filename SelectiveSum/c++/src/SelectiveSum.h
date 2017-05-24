dba0d5a21cbddf66a3948ec4e2f258db074e4f89
rwbzx <zhang_x_029@yahoo.com>
2017-05-25 00:07:28 +0800

    Add [SelectiveSum] algo implementaion

---
+++ SelectiveSum/c++/src/SelectiveSum.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifdef DLL_EXPORT
+#define DLL_API __declspec(dllexport)
+#else
+#define DLL_API __declspec(dllimport)
+#endif
+
+bool DLL_API SelectiveSum(int * input, int num, int sum);
